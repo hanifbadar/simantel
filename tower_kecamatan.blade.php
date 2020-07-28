e0ca9a04fe21bb620ebbb1ba36d38f68d8c00cbc
Qindilsyah Abrar Hanif <37163851+hanifbadar@users.noreply.github.com>
2020-07-28 10:35:55 +0700

    Update tower_kecamatan.blade.php
    tambahan kolom izin listrik pagar papan petugas hp petugas shelter dan genset
    kurang provider

--- tower_kecamatan.blade.php
+++ tower_kecamatan.blade.php
@@ -13,7 +13,13 @@
       <th scope="col">KOORDINAT</th>
       <th scope="col">KETINGGIAN</th>
       <th scope="col">PEMILIK TANAH</th>
-      
+      <th scope="col">IZIN TOWER</th>
+      <th scope="col">LISTRIK</th>
+      <th scope="col">PAGAR TOWER</th>
+      <th scope="col">PAPAN NAMA TOWER</th>
+      <th scope="col">PETUGAS</th>
+      <th scope="col">HP PETUGAS</th>
+      <th scope="col">SHELTER DAN GENSET</th>
     </thead>
 
   @foreach ($data as $x)
@@ -23,6 +29,13 @@
       <td>{{$x->koordinat}}</td>
       <td>{{$x->ketinggian_meter}}</td>
       <td>{{$x->pemilik_tanah}}</td>
+      <td>{{$x->izin_tower}}</td>
+      <td>{{$x->listrik}}</td>
+      <td>{{$x->pagar_tower}}</td>
+      <td>{{$x->papan_nama}}</td>
+      <td>{{$x->petugas}}</td>
+      <td>{{$x->hp_petugas}}</td>
+      <td>{{$x->shelter_genset}}</td>
   </tr>
   @endforeach
 
